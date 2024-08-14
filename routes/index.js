e9ed1f1ee2bed2c878d62c11f801e782c0d56456
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-08-14 21:52:54 +0100

    add route handler for author list

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const router = express.Router()
 
 const book_controller = require("../controllers/bookController")
+const author_controller = require("../controllers/authorController")
 
 router.get('/', book_controller.index)
 
@@ -9,7 +10,7 @@ router.get('/books', book_controller.book_list)
 
 router.get('/books/:id', book_controller.book_detail)
 
-// router.get('/authors', )
+router.get('/authors', author_controller.author_list)
 
 // router.get('/genres',)
 
