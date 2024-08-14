808d23a3e6a85170186c9c25a0ca9232b775554e
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-08-14 17:05:47 +0100

    add function handler for retrieving book details in bookController

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -35,3 +35,39 @@ exports.book_list = asyncHandler( async(req, res, next) => {
         book_list: rows,
     })
 })
+
+exports.book_detail = asyncHandler( async(req, res, next) => {
+    console.log("book detail")
+
+    const bookID = req.params.id
+
+    const queryOne =   `SELECT title, family_name || ', ' || first_name AS full_name, summary, isbn, 
+                        ARRAY_AGG (name) genres
+                        FROM books
+                        INNER JOIN authors
+                        ON authors.author_id = books.author_id
+                        INNER JOIN books_genre
+                        ON books.book_id = books_genre.book_id
+                        INNER JOIN genres
+                        ON genres.genre_id = books_genre.genre_id
+                        WHERE books.book_id = $1
+                        GROUP BY title, family_name, first_name, summary, isbn;`
+
+    const queryTwo =   `SELECT bk_instance_id, imprint, status, due_back FROM book_instances
+                        INNER JOIN books
+                        ON books.book_id = book_instances.book_id
+                        WHERE books.book_id = $1;`
+                        
+    const values = [ bookID ]
+
+    const [ book, bookInstances ] = await Promise.all([
+        await pool.query(queryOne, values),
+        await pool.query(queryTwo, values)
+    ])
+
+    res.render("book_detail", {
+        title: book.rows[0].title,
+        book: book.rows[0],
+        book_instances: bookInstances.rows
+    })
+})
