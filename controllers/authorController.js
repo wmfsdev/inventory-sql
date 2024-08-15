51fe28234b7ee270531a192685ef7b9744d25628
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-08-15 15:29:59 +0100

    add function handler for retrieving author details in authorController

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -18,3 +18,23 @@ exports.author_list = asyncHandler( async(req, res, next) => {
         author_list: rows
     })
 })
+
+exports.author_detail = asyncHandler( async(req, res, next) => {
+    console.log("author detail")
+    const bookID = req.params.id
+
+    const text =   `SELECT family_name || ', ' || first_name AS full_name,
+                    TO_CHAR(date_of_birth, 'Mon dd, YYYY') AS dob,
+                    TO_CHAR(date_of_death, 'Mon dd, YYYY') AS dod,
+                    title, book_id, summary FROM authors
+                    INNER JOIN books
+                    ON authors.author_id = books.author_id
+                    WHERE authors.author_id = $1;`
+  
+    const { rows } = await pool.query(text, [bookID])
+    console.log(rows)
+    res.render("author_detail", {
+        title: "Author Detail",
+        author: rows
+    })
+})
