9688120360e836e6f61792dd5c8c02c3cf4266ee
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-08-15 17:11:43 +0100

    add route handler for genre list

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 const book_controller = require("../controllers/bookController")
 const author_controller = require("../controllers/authorController")
+const genre_controller = require("../controllers/genreController")
 
 router.get('/', book_controller.index)
 
@@ -14,7 +15,7 @@ router.get('/authors', author_controller.author_list)
 
 router.get('/authors/:id', author_controller.author_detail)
 
-// router.get('/genres',)
+router.get('/genres', genre_controller.genre_list)
 
 // router.get('/bookinstances', )
 
