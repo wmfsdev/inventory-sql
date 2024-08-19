7266f09cd32917847326c716f89d1ced9319763a
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-08-19 22:33:36 +0100

    add function handler for deleting authors

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -26,7 +26,7 @@ exports.author_detail = asyncHandler( async(req, res, next) => {
     const text =   `SELECT family_name || ', ' || first_name AS full_name,
                     TO_CHAR(date_of_birth, 'Mon dd, YYYY') AS dob,
                     TO_CHAR(date_of_death, 'Mon dd, YYYY') AS dod,
-                    title, book_id, summary FROM authors
+                    title, authors.author_id, book_id, summary FROM authors
                     INNER JOIN books
                     ON authors.author_id = books.author_id
                     WHERE authors.author_id = $1;`
@@ -37,4 +37,28 @@ exports.author_detail = asyncHandler( async(req, res, next) => {
         title: "Author Detail",
         author: rows
     })
+})
+
+exports.author_delete = asyncHandler( async(req, res, next) => {
+
+    const authorID = req.params.id
+
+    const text =    `SELECT COUNT(*) FROM authors
+                     INNER JOIN books
+                     ON authors.author_id = books.author_id
+                     WHERE authors.author_id = $1;`
+
+    const { rows } = await pool.query(text, [authorID])
+
+    if (rows[0].count === "0") {
+    
+        const text =    `DELETE FROM authors
+                         WHERE authors.author_id = $1;`
+
+        const { rows } = await pool.query(text, [authorID])
+
+        res.redirect(`/authors/${authorID}`)
+    } else {
+        res.redirect(`/authors/${authorID}`)
+    }
 })
