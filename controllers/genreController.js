0327f79d51e9a2967cb1ddb02133399b6a4fcc6c
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-08-27 21:30:52 +0100

    add POST function handler for creating genre

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,9 +1,9 @@
 
 const asyncHandler = require("express-async-handler")
 const pool = require("../db/pool")
+const { body, validationResult } = require("express-validator")
 
 exports.genre_list = asyncHandler( async(req, res, next) => {
-    console.log("genre list")
 
     const text =    `SELECT * FROM genres;`
     const { rows } = await pool.query(text)
@@ -28,11 +28,48 @@ exports.genre_detail = asyncHandler(  async(req, res, next) => {
 
     const { rows } = await pool.query(text, [genreID])
 
-    console.log(rows)
+    if (rows.length === 0) {
 
-    res.render("genre_detail", {
-        title: "Genre Detail",
-        genre_name: rows[0].name,
-        genre_books: rows
+        const text = `SELECT name FROM genres WHERE genre_id = $1;`
+        const { rows } = await pool.query(text, [genreID])
+
+        res.render("genre_detail", {
+            genre_name: rows[0].name,
+            genre_books: rows
+        })
+    } else {
+        res.render("genre_detail", {
+            title: "Genre Detail",
+            genre_name: rows[0].name,
+            genre_books: rows
+        })
+    }
+})
+
+exports.genre_create_get = asyncHandler( async(req, res, next) => {
+    res.render("genre_form", {
+        title: "Create Genre"
     })
 })
+
+exports.genre_create_post = [
+
+    body("name", "Genre name must contain at least 3 characters")
+        .trim()
+        .isLength({ min: 3 })
+        .escape(),
+
+    asyncHandler( async(req, res, next) => {
+
+        const newGenre = req.body.genre
+    
+        const text = `INSERT INTO genres (name) VALUES ($1);`
+    
+        const { rows } = await pool.query(text, [newGenre])
+    
+        res.render("genre_list", {
+            title: "Genre List",
+            genre_list: rows
+        })
+    })
+]
