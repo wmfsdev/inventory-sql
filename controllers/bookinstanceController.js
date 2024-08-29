305e10bedd760b62fb5a538a52ca59177b6ad7dd
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-08-29 21:57:40 +0100

    add POST and GET function handlers for book instance creation

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -1,17 +1,15 @@
 
 const asyncHandler = require("express-async-handler")
 const pool = require("../db/pool")
+const { body, validationResult } = require("express-validator")
 
 exports.bkinstance_list = asyncHandler( async(req, res, next) => {
-    console.log("book instance list")
 
     const text =    `SELECT imprint, TO_CHAR(due_back, 'Mon, dd, YYYY') AS due_back, status, title, bk_instance_id FROM book_instances
                      INNER JOIN books
                      ON book_instances.book_id = books.book_id;`
 
     const { rows } = await pool.query(text)
-
-    console.log(rows)
 
     res.render("bkinstance_list", {
         title: "Book Instances",
@@ -20,7 +18,6 @@ exports.bkinstance_list = asyncHandler( async(req, res, next) => {
 })
 
 exports.bkinstance_detail = asyncHandler( async(req, res, next) => {
-    console.log("book instance detail")
 
     const bkinstanceID = req.params.id
     const text =    `SELECT bk_instance_id, books.book_id, title, imprint, status, TO_CHAR(due_back, 'Mon, dd, YYYY') AS due_back
@@ -36,7 +33,6 @@ exports.bkinstance_detail = asyncHandler( async(req, res, next) => {
 })
 
 exports.bkinstance_delete = asyncHandler( async(req, res, next) => {
-    console.log("book instance delete")
 
     const bkinstanceID = req.params.id
     const text =    `DELETE FROM book_instances
@@ -62,3 +58,62 @@ exports.auth_bkinstance_list = asyncHandler( async(req, res, next) => {
         bkinstance_list: rows
     })
 })
+
+exports.bkinstance_create_get = asyncHandler( async( req, res, next) => {
+
+    const text = `SELECT title, book_id FROM books;`
+
+    const { rows } = await pool.query(text)
+
+    res.render("bookinstance_form", {
+        title: "Create Book Instance",
+        book_list: rows
+    })
+})
+
+exports.bkinstance_create_post = [
+
+    body("book", "Book must not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("imprint", "Author must not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("due_back", "Summary must not be empty.")
+        .optional({ values: "falsy" })
+        .isISO8601()
+        .toDate(),
+    body("status", "Status must not be empty")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+
+    asyncHandler( async( req, res, next) => {
+
+        const errors = validationResult(req)
+
+        const bkinstance = {
+            bookid: req.body.book,
+            imprint: req.body.imprint,
+            dueBack: req.body.due_back,
+            status: req.body.status
+        }
+
+        if (errors.isEmpty()) {
+            const text =    `INSERT INTO book_instances (book_id, imprint, status, due_back)
+                                VALUES ($1, $2, $3, $4);`
+            const values = [ Number.parseInt(bkinstance.bookid), bkinstance.imprint, bkinstance.status, bkinstance.dueBack ]
+
+            await pool.query(text, values)
+
+            res.redirect("/bookinstances/")
+        } else {
+            res.render("bookinstance_form", {
+                title: "Create Book Instance",
+                errors: errors.array()
+            })
+        }
+    })
+]
